cd48227e828d9ba28cf67cef4339b30737f08161
daramayo90 <da.aramayo1990@gmail.com>
2022-04-29 18:03:40 -0500

    Finish v1.0

--- canvas.js
+++ canvas.js
@@ -3,55 +3,27 @@ let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
+})
+
 // Super object passing a tons of methods and functions to draw within our canvas
 let c = canvas.getContext('2d');
-/*
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
-
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(400, 100, 100, 100);
-
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(300, 300, 100, 100);
-
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa34a3"
-c.stroke();
-
-// Arc/Circle
-
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.strokeStyle = 'blue';
-c.stroke();
-
-
-for (let i=0 ; i<400 ; i++) {
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
-}
-*/
-
-/*
-
-*/
 
 const mouse = {
     x: undefined,
     y: undefined
 }
 
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
 const maxRadius = 40;
-//const minRadius = 2;
 
 const colorArray = [
     '#F0E3D7',
@@ -59,20 +31,8 @@ const colorArray = [
     '#031E1D',
     '#BF1E0F',
     '#E0D0C1'
-]
+];
 
-window.addEventListener('mousemove', (e) => {
-    mouse.x = e.x;
-    mouse.y = e.y;
-    console.log(mouse)
-});
-
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    init();
-})
 
 class Circle {
     constructor(x, y, dx, dy, radius) {
@@ -119,11 +79,9 @@ class Circle {
 }
 
 
-
-
 let circleArray = [];
 
-function init () {
+function init() {
     circleArray = [];
 
     for (let i = 0; i < 800; i++) {
